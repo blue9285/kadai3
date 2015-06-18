0c6fe160705f1e1dd2961d17d8f2c5b15773cefb
Daiki Aoki <blue928527@yahoo.co.jp>
2015-06-18 17:37:08 +0900

    fixed9

---
+++ test2.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEST1=$(./kadai2 20 90)
+echo "$TEST1"
+if [ "$TEST1" -eq 10 ]; then
+    echo "OK"
+else
+    echo "NG"
+    exit 1
+fi
+
+TEST2=$(./kadai2 22 33)
+echo "TEST2"
+if [ "$TEST2" -eq 11 ]; then
+    echo "OK"
+else
+    echo "NG"
+    exit 1
+fi
