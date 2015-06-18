3d67849951caeb76cd13f7895f9fc4dbc4a0b3d8
Daiki Aoki <blue928527@yahoo.co.jp>
2015-06-18 17:08:55 +0900

    fixed6

--- test.bash
+++ test.bash
@@ -18,8 +18,8 @@ echo 4 > $tmp-ans
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 echo 5 > $tmp-ans
-./kadai2 5 10 > $tmp-out || ERROR_EXIT "TEST1-1"
-diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+./kadai2 5 10 > $tmp-out || ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-2"
 
 echo OK
 rm -f $tmp-*
