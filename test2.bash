099ec60ad7d97528dc75d918c547d7af021d8afc
Daiki Aoki <blue928527@yahoo.co.jp>
2015-06-18 17:42:20 +0900

    fixed10

--- test2.bash
+++ test2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEST1=$(./kadai2 20 90)
+TEST1=$(./kadai2.2 20 90)
 echo "$TEST1"
 if [ "$TEST1" -eq 10 ]; then
     echo "OK"
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-TEST2=$(./kadai2 22 33)
+TEST2=$(./kadai2.2 22 33)
 echo "TEST2"
 if [ "$TEST2" -eq 11 ]; then
     echo "OK"
